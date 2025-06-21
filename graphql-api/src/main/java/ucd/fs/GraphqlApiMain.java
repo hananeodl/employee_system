595f5b4ce4038615dbac6a3ffd191cf27b21652e
GitAmina <nianeaminata11@gmail.com>
2025-06-21 13:21:49 +0100

    Ajout modules dans pom.xml

--- graphql-api/src/main/java/ucd/fs/GraphqlApiMain.java
+++ graphql-api/src/main/java/ucd/fs/GraphqlApiMain.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "ucd.fs.client")
-public class Main {
+public class GraphqlApiMain {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(GraphqlApiMain.class, args);
     }
 }
